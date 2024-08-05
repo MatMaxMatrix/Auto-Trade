99dbada3444978283986a74802a7be8804bfb832
Mobin Azimipanah <azimipanah.mobin@gmail.com>
2024-08-06 00:33:57 +0200

    test

--- general.py
+++ general.py
@@ -12,6 +12,31 @@ print(hasattr(my_instance, 'my_attribute'))  # Outputs: True
 # Check if 'non_existent_attribute' exists in my_instance
 print(hasattr(my_instance, 'non_existent_attribute'))  # Outputs: False
 # %%
+class MyClass:
+    def __init__(self):
+        self.my_attribute = "Hello, world!"
+
+    def my_method(self):
+        return "Hello from my_method!"
+
+# Create an instance of MyClass
+my_instance = MyClass()
+
+# Check if 'my_attribute' exists in my_instance
+print(hasattr(my_instance, 'my_attribute'))  # Outputs: True
+
+# Check if 'my_method' exists in my_instance
+print(hasattr(my_instance, 'my_method'))  # Outputs: True
+
+# Check if 'non_existent_attribute' exists in my_instance
+print(hasattr(my_instance, 'non_existent_attribute'))  # Outputs: False
+
+
+
+
+
+
+#%%
 import copy
 
 # A list of lists
