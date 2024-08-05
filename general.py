d29a942f92aee3da1d6bc86e7287c06fad3c1e6c
Mobin Azimipanah <azimipanah.mobin@gmail.com>
2024-08-06 00:31:05 +0200

    test

--- general.py
+++ general.py
@@ -12,3 +12,21 @@ print(hasattr(my_instance, 'my_attribute'))  # Outputs: True
 # Check if 'non_existent_attribute' exists in my_instance
 print(hasattr(my_instance, 'non_existent_attribute'))  # Outputs: False
 # %%
+import copy
+
+# A list of lists
+original = [[1], [2], [3]]
+
+# Create a shallow copy
+shallow_copy = copy.copy(original)
+
+# Create a deep copy
+deep_copy = copy.deepcopy(original)
+
+# Modify the original list
+original[0][0] = 'a'
+
+print(original)        # Outputs: [['a'], [2], [3]]
+print(shallow_copy)    # Outputs: [['a'], [2], [3]]
+print(deep_copy)       # Outputs: [[1], [2], [3]]
+# %%
