e4350a95872989a70cf755ebaa1f539f9de3ede5
AZIMIPANAH Mobin - Stagiaire <mobin.azimipanah@crf.canon.fr>
2024-08-06 09:42:32 +0200

    test

--- general.py
+++ general.py
@@ -55,3 +55,20 @@ print(original)        # Outputs: [['a'], [2], [3]]
 print(shallow_copy)    # Outputs: [['a'], [2], [3]]
 print(deep_copy)       # Outputs: [[1], [2], [3]]
 # %%
+
+
+#re.findall:This function searches a string for all occurrences of a pattern and returns them as a list of strings.
+
+questions_text = """1. What is the capital of France?
+
+2. Who wrote "Romeo and Juliet"?
+
+3. What is the chemical symbol for gold?
+
+4. What year did World War II end?"""
+
+import re
+
+questions = re.findall(r'\d+\.\s*(.*?)(?=\n\d+\.|\Z)', questions_text, re.DOTALL) #\d+\.: Matches 1., 2., 3.' and \s*: Matches any spaces after the period. and (.*?): Captures the question text. AND (?=\n\d+\.|\Z): Ensures we stop capturing at the next question or the end of the text.
+print(questions)
+# %%
